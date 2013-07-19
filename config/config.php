4c707079005253198424ea5559d1e3c13f68f923
root <root@owncloud>
2013-07-18 20:45:49 -0600

    Primera versión de la NAC

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+$CONFIG = array (
+  'instanceid' => '51e83258c5135',
+  'passwordsalt' => '6c6ae3ef35e2fc58e2df9df7650eba',
+  'datadirectory' => '/var/www/owncloud/data',
+  'dbtype' => 'sqlite3',
+  'version' => '5.0.15',
+  'installed' => true,
+  'ldapIgnoreNamingRules' => false,
+);
